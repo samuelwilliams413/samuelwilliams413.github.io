6edb4e99a2a87b1b6e9aad910ea57004db24f1aa
Samuel Williams <samuel.williams@digi.com>
2020-06-15 16:32:58 +1000

    temporary commit

---
+++ tg/peacemaker/gurps_readthrough.sh
@@ -0,0 +1,100 @@
+https://thetrove.net/Books/GURPS%20Classic/Old%20West%20-%202nd%20Edition.pdf
+
+
+Trading Post
+
+
+
+Fur
+Meat
+Tools
+Weapons
+Liquor & Supplies
+Saloon
+Doctor
+
+
+
+Saloon 1 & 2
+Blacksmith
+Shebang (general store)
+Newspaper
+
+
+
+Cost of living - 11
+Equipment list - 37
+
+
+
+Circus
+
+
+
+Buffalo Hunters
+Confidence Men
+Cowboys
+Blacksmiths
+Carpenters
+Masons
+Pinkertons
+Doctors
+Drunkards
+Tenderfeet
+Entertainers
+Trailblazers
+Expressmen
+Gamblers
+Gunslingers
+Indian Agents
+Laborers
+Lawmen
+Merchants and Businessmen
+Mountain Men
+Outlaws
+Evangelist
+Pioneers and Homesteaders
+Professionals
+Prospectors
+Scientists and Engineers
+Scouts
+“Soiled Doves”
+Soldiers
+
+
+Pinkertons are mercenaries who work for Allan Pinkerton’s
+detective agency. They bring many outlaws to justice, sometimes fighting against corrupt local officials or citizens who idolize certain outlaws. Many express companies and railroads hire
+detectives to protect company property, solve cases, or guard
+valuable shipments.
+Detectives benefit from Alertness and Intuition. Reputations
+are common. A detective’s agency may be a Patron.
+Overconfidence and Stubbornness are typical disadvantages. Duties to employers and agencies are required. Enemies
+from past cases are likely. Agency detectives typically have
+Legal Enforcement Powers (10 points); they can make arrests
+and engage in covert investigations. Some Pinkertons have a
+Delusion that their Legal Enforcement Powers allow them to kill
+with relative impunity.
+Essential skills include Criminology, Guns, and Streetwise.
+Acting, Area Knowledge, Disguise, Law, and Shadowing help.
+See also Expressmen and Lawmen.
+
+
+
+
+
+
+
+
+
+Saloon 1
+Saloon 2
+Blacksmith
+Shebang
+The celestial (chinaman) (Doctor)
+Fur Trader
+Butcher
+Newspaper
+Flophouse
+
+
+Uncanny
